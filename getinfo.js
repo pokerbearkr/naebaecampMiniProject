750712cf7bda45ccf36d5f6823e24ec800175ac5
janghwal <jhp337037@gmail.com>
2025-02-19 17:45:36 +0900

    guest-book 분리 main_guestbook, main_guestbook2 삭제

--- getinfo.js
+++ getinfo.js
@@ -72,71 +72,7 @@ export async function getDetail(docId) {
 // 메인에 사용
 export async function getMain() {
 
-    $("#postbtn").click(async function () {
-        let title = $('#title').val();
-        let content = $('#content').val();
-        let currentDate = new Date();
-
-        let doc = {
-            'title': title,
-            'content': content,
-            'date': currentDate,
-        };
-        await addDoc(collection(db, "guestbooks"), doc);
-        alert('방명록 작성 완료!')
-        window.location.reload();
-    })
-
-    $(document).on("click", ".delete-button", async function () {
-        let docId = $(this).attr("data-id"); // 클릭한 버튼(방명록데이터터)의 데이터 ID 가져오기
-
-        if (confirm("방명록을 지우시겠습니까?")) {
-            await deleteDoc(doc(db, "guestbooks", docId)); // Firestore에서 삭제
-            $(`div[data-id='${docId}']`).remove(); // 화면에서 삭제
-            alert("방명록이 삭제되었습니다!");
-        }
-    });
-
     try {
-
-        const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
-
-        const q_guestbooks = query(collection(db, "guestbooks"), orderBy("date", "desc"));
-        const querySnapshot_guestbooks = await getDocs(q_guestbooks);
-
-        querySnapshot_guestbooks.forEach((doc) => {
-            let guestbooks = doc.data();
-
-            let date = guestbooks['date'];
-
-            let date_temp1 = date.seconds * 1000 + Math.floor(date.nanoseconds / 1000000);
-            let date_temp2 = new Date(date_temp1);
-            console.log(date);
-
-            let temp_html = `<div class="card mb-3" data-id="${doc.id}">
-      <div class="card-header d-flex justify-content-between">
-        <span class="fw-bold">${guestbooks.title}</span>
-        <button class="delete-button" type="button" data-id="${doc.id}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-x-square" viewBox="0 0 16 16">
-                                <path
-                                    d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
-                                <path
-                                    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                            </svg>
-         </button>
-      </div>
-      <div class="card-body">
-        <p class="card-text">${guestbooks.content}</p>
-        <p><small style = "float: right;" class="text-muted">${new Intl.DateTimeFormat('ko-KR', options).format(date_temp2)}</small><p>
-      </div>
-    </div>`;
-
-            $('#makeguestbook').append(temp_html);
-        });
-
-
-
         let C_div = true;
 
         // 팀장 쿼리 실행
@@ -218,3 +154,74 @@ export async function getMain() {
     }
 }
 
+export async function getGuestbook() {
+    
+    $("#postbtn").click(async function () {
+        let title = $('#title').val();
+        let content = $('#content').val();
+        let currentDate = new Date();
+
+        let doc = {
+            'title': title,
+            'content': content,
+            'date': currentDate,
+        };
+        await addDoc(collection(db, "guestbooks"), doc);
+        alert('방명록 작성 완료!')
+        window.location.reload();
+    })
+
+    $(document).on("click", ".delete-button", async function () {
+        let docId = $(this).attr("data-id"); // 클릭한 버튼(방명록데이터터)의 데이터 ID 가져오기
+
+        if (confirm("방명록을 지우시겠습니까?")) {
+            await deleteDoc(doc(db, "guestbooks", docId)); // Firestore에서 삭제
+            $(`div[data-id='${docId}']`).remove(); // 화면에서 삭제
+            alert("방명록이 삭제되었습니다!");
+        }
+    });
+
+    try {
+
+        const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit' };
+
+        const q_guestbooks = query(collection(db, "guestbooks"), orderBy("date", "desc"));
+        const querySnapshot_guestbooks = await getDocs(q_guestbooks);
+
+        querySnapshot_guestbooks.forEach((doc) => {
+            let guestbooks = doc.data();
+
+            let date = guestbooks['date'];
+
+            let date_temp1 = date.seconds * 1000 + Math.floor(date.nanoseconds / 1000000);
+            let date_temp2 = new Date(date_temp1);
+            console.log(date);
+
+            let temp_html = `<div class="card mb-3" data-id="${doc.id}">
+      <div class="card-header d-flex justify-content-between">
+        <span class="fw-bold">${guestbooks.title}</span>
+        <button class="delete-button" type="button" data-id="${doc.id}">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-x-square" viewBox="0 0 16 16">
+                                <path
+                                    d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+                                <path
+                                    d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                            </svg>
+         </button>
+      </div>
+      <div class="card-body">
+        <p class="card-text">${guestbooks.content}</p>
+        <p><small style = "float: right;" class="text-muted">${new Intl.DateTimeFormat('ko-KR', options).format(date_temp2)}</small><p>
+      </div>
+    </div>`;
+
+            $('#makeguestbook').append(temp_html);
+        });
+
+
+    } catch (error) {
+        console.error("Firestore 데이터 로딩 중 오류 발생:", error);
+    }
+
+}
